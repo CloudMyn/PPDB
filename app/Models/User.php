ec2982cffdd1097c289ed8d7f290ee9368b1598c
cloudmyn <cloudmyn46@gmail.com>
2025-01-03 15:16:07 +0800

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,7 +66,6 @@ class User extends Authenticatable implements HasAvatar, FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        dd($this->role);
         return $this->role === 'SISWA' && $panel->getId() == 'siswa' || $this->role === 'ADMIN' && $panel->getId() == 'admin' || $this->role === 'KEPSEK' && $panel->getId() == 'kepsek';
     }
 
